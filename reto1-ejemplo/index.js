330f41c6890136d6c47a5145862c699bb96e2947
RolandoAndrade <rolandoandradefernandez@gmail.com>
2019-01-29 10:04:10 -0400

    Initial commit

---
+++ reto1-ejemplo/index.js
@@ -0,0 +1,5 @@
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+ctx.fillColor="#000";
+ctx.fillRect(0,0,100,100);
---
+++ reto1-ejemplo/shapes.js
@@ -0,0 +1,15 @@
+class Circle
+{
+    constructor(x, y, radius, color)
+    {
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+        this.color=color;
+    }
+
+    draw()
+    {
+        
+    }
+}
